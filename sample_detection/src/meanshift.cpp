b3fe71b4bbe49f75efdbb50a0ac79955419a3aa8
Rasmus Raag <rasmus.raag@gmail.com>
2013-01-28 15:51:54 +0200

    added patch for gazebo

--- sample_detection/src/meanshift.cpp
+++ sample_detection/src/meanshift.cpp
@@ -2,13 +2,16 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/photo/photo.hpp"
+#include <string>
+#include <sstream>
 
 using namespace cv;
 using namespace std;
 
-Mat img, res, hsv, element, mask;
+Mat img, res, hsv, element, mask, thresholded_image, threshold_helper;
 vector<Mat> hsv_planes;
-int sp = 20.0, sr=20.0;
+int sp = 20, sr=20;
+int lower_threshold = 50, upper_threshold = 75;
 
 void spCallback(int,void*);
 static void onMouse( int event, int x, int y, int, void* );
@@ -39,10 +42,13 @@ int main(int argc, char** argv)
    cvtColor(hsv, img, CV_HSV2BGR);
 
    namedWindow( "window", CV_WINDOW_NORMAL );
-   namedWindow( "maskwindow", CV_WINDOW_NORMAL );
    createTrackbar( "Spatial radius", "window", &sp, 255, NULL );
    createTrackbar( "Color radius", "window", &sr, 255, NULL );
    setMouseCallback( "window", onMouse, 0 );
+
+   namedWindow( "maskwindow", CV_WINDOW_NORMAL );
+   createTrackbar( "Lower thresh", "maskwindow", &lower_threshold, 255, NULL );
+   createTrackbar( "Upper thresh", "maskwindow", &upper_threshold, 255, NULL );
 
    //pyrMeanShiftFiltering( img, res, sp, sr, 3);
 //   imwrite("output.png", res);
@@ -57,7 +63,7 @@ void spCallback(int a, void * b)
 {
     pyrMeanShiftFiltering( img, res, (float)sp, (float)sr, 3);
 //    pyrSegmentation(res, res, )
-    cvtColor(res, hsv, CV_HSV2BGR);
+    cvtColor(res, hsv, CV_BGR2HSV);
     split(hsv, hsv_planes );
     imshow( "window", hsv_planes[0] );
 //    imshow( "window", res );
@@ -67,18 +73,44 @@ static void onMouse( int event, int x, int y, int, void* )
 {
 	switch (event) {
 		case CV_EVENT_RBUTTONUP:
-			cvtColor(res, hsv, CV_HSV2BGR);
-			split(hsv, hsv_planes );
-			floodFill(hsv, mask, Point(x,y), 255, 0,
-					cv::Scalar(), cv::Scalar(10,150,250),
-					4 + (255 << 8) + cv::FLOODFILL_MASK_ONLY);
+//			cvtColor(res, hsv, CV_BGR2HSV);
+			split(img, hsv_planes );
+//			floodFill(hsv, mask, Point(x,y), 255, 0,
+//					cv::Scalar(), cv::Scalar(20,250,250),
+//					4 + (255 << 8) + cv::FLOODFILL_MASK_ONLY);
+//			imshow( "maskwindow", mask );
+			imshow( "maskwindow", hsv_planes[1] );
 			break;
 		case CV_EVENT_LBUTTONUP:
 			spCallback(0, NULL);
+			imshow( "maskwindow", mask );
 			break;
+		case CV_EVENT_MBUTTONUP:
+		{
+			cvtColor(res, hsv, CV_BGR2HSV);
+			split(hsv, hsv_planes );
+			threshold(hsv_planes[0], thresholded_image, lower_threshold, 0, CV_THRESH_TOZERO);
+			threshold(hsv_planes[0], threshold_helper, upper_threshold, 0, CV_THRESH_TOZERO);
+			thresholded_image = thresholded_image - threshold_helper;
+			threshold(thresholded_image, thresholded_image, 1, 255, CV_THRESH_BINARY);
+			imshow("maskwindow", thresholded_image);
+
+			stringstream message;
+
+			message << "HSV: "
+					<< (unsigned int) hsv_planes[0].at<unsigned char>(x,y) << ","
+					<< (unsigned int) hsv_planes[1].at<unsigned char>(x,y) << ","
+					<< (unsigned int) hsv_planes[2].at<unsigned char>(x,y);
+			res.copyTo(threshold_helper);
+			putText(threshold_helper, message.str(), cvPoint(60,60),
+					FONT_HERSHEY_COMPLEX_SMALL, 1.0, cvScalar(0,100,250), 2);
+			imshow( "window", threshold_helper );
+
+			break;
+		}
 		default:
 			break;
 	}
 
-	imshow( "maskwindow", mask );
+
 }
