02259c8b1255bbfc20da0b2778909c87106cbbbc
Rasmus Raag <rasmus.raag@gmail.com>
2013-02-03 12:28:15 +0200

    Improved image segmentation

--- sample_detection/src/meanshift.cpp
+++ sample_detection/src/meanshift.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 Mat img, res, hsv, element, mask, thresholded_image, threshold_helper;
 vector<Mat> hsv_planes;
-int sp = 20, sr=20;
+int sp = 0, sr=0;
 int lower_threshold = 50, upper_threshold = 75;
 
 void spCallback(int,void*);
@@ -48,6 +48,7 @@ int main(int argc, char** argv)
    setMouseCallback( "window", onMouse, 0 );
 
    namedWindow( "maskwindow", CV_WINDOW_NORMAL );
+
    createTrackbar( "Lower thresh", "maskwindow", &lower_threshold, 180, NULL );
    createTrackbar( "Upper thresh", "maskwindow", &upper_threshold, 180, NULL );
 
@@ -96,6 +97,40 @@ static void onMouse( int event, int x, int y, int, void* )
 			threshold(thresholded_image, thresholded_image, 1, 255, CV_THRESH_BINARY);
 			imshow("maskwindow", thresholded_image);
 
+			vector<vector<Point> > contours;
+			vector<Vec4i> hierarchy;
+			findContours( thresholded_image, contours, hierarchy, CV_RETR_TREE,
+					CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+			//exclude small contours
+			double min_area = 2000, max_area=200000;
+			vector<bool> big_contours;
+			big_contours.reserve(contours.size());
+			for (int i = 0; i < contours.size(); i++)
+			{
+				if(contourArea(contours[i]) > min_area && contourArea(contours[i]) < max_area)
+					big_contours.push_back(true);
+				else
+					big_contours.push_back(false);
+			}
+			/// Draw contours
+			RNG rng(12345);
+//			Mat drawing = Mat::zeros( thresholded_image.size(), CV_8UC3 );
+			Mat drawing;
+			img.copyTo(drawing);
+			for( int i = 0; i< contours.size(); i++ )
+			{
+				if(!big_contours[i])
+					continue;
+				Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+				drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+			}
+
+			/// Show in a window
+			namedWindow( "Contours", CV_WINDOW_NORMAL );
+			imshow( "Contours", drawing );
+
+
 //			stringstream message;
 //
 //			message << "HSV: "
@@ -134,6 +169,28 @@ static void onMouse( int event, int x, int y, int, void* )
 
 			int prevalent_hue[2];
 			minMaxIdx( hue_hist, 0, 0, 0, prevalent_hue, Mat() );
+			// TODO: hue is circular
+			int min1 = prevalent_hue[0]-20, min2 = prevalent_hue[0]+20;
+			// find the left limit of the prevalent color
+			for( int i = prevalent_hue[0]-1; i > 0 && i > prevalent_hue[0]-20; i-- )
+			{
+				if(hue_hist.at<float>(i) < hue_hist.at<float>(prevalent_hue[0])*0.2
+						&& hue_hist.at<float>(i) > hue_hist.at<float>(i+1))
+				{
+					min1 = i;
+					break;
+				}
+			}
+			// find the right limit of the prevalent color
+			for( int i = prevalent_hue[0]+1; i < histSize && i < prevalent_hue[0]+20; i++ )
+			{
+				if(hue_hist.at<float>(i) < hue_hist.at<float>(prevalent_hue[0])*0.2
+						&& hue_hist.at<float>(i) > hue_hist.at<float>(i-1))
+				{
+					min2 = i;
+					break;
+				}
+			}
 //			lower_threshold = std::max(prevalent_hue[0]-20, 0);
 //			upper_threshold = std::min(prevalent_hue[0]+20, 180);
 //			setTrackbarPos("Lower thresh", "maskwindow", std::max(prevalent_hue[0]-20, 0));
@@ -152,8 +209,9 @@ static void onMouse( int event, int x, int y, int, void* )
 							   Point( bin_w*(i), hist_h - cvRound(hue_hist.at<float>(i)) ),
 							   color, 2, 8, 0  );
 			}
-			setTrackbarPos("Lower thresh", "maskwindow", std::max(prevalent_hue[0]-20, 0));
-			setTrackbarPos("Upper thresh", "maskwindow", std::min(prevalent_hue[0]+20, 180));
+			setTrackbarPos("Lower thresh", "maskwindow", min1);
+			setTrackbarPos("Upper thresh", "maskwindow", min2);
+
 			/// Display
 //			namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
 //			imshow("calcHist Demo", histImage );
